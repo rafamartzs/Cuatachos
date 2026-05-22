c3689e63067d481a7804758ef156e63a39af1396
rafamartzs <rafael.martzs@gmail.com>
2026-05-22 14:06:27 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -451,7 +451,7 @@ if nombre and nombre != "🏆 Salón de la Fama":
 
     fig2, ax2 = plt.subplots(figsize=(10,5))
 
-    ax2.bar(df_mes3_sorted["Nombre"], df_mes2_sorted["Total Mes2"], color=colors)
+    ax2.bar(df_mes2_sorted["Nombre"], df_mes2_sorted["Total Mes2"], color=colors)
     
     ax2.axhline(y=1000, color='red', linestyle='--', linewidth=2, label='Objetivo 1000 min')
     ax2.legend()
